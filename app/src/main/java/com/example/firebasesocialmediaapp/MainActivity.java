14ba9c4b2a8be9fb6d28408aad022ad523b3b718
Aliasgar Nasir <nasiraliasgar@gmail.com>
2020-04-29 08:34:08 +0530

    SignUp and SignIn using Firebase. Uploading images to the server Using Firebase.

---
+++ app/src/main/java/com/example/firebasesocialmediaapp/MainActivity.java
@@ -0,0 +1,117 @@
+package com.example.firebasesocialmediaapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText edtEmail,edtUserName,edtPassword;
+    private Button btnSignUp,btnSignIn;
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        edtEmail = findViewById(R.id.edtEmail);
+        edtUserName = findViewById(R.id.edtUserName);
+        edtPassword = findViewById(R.id.edtPassword);
+
+        btnSignUp = findViewById(R.id.btnSignUp);
+        btnSignIn = findViewById(R.id.btnSignIn);
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signUp();
+            }
+        });
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn();
+            }
+        });
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        if (currentUser != null){
+            //Transition to Another Activity
+            transitionToSocialMediaActivity();
+        }
+    }
+
+    private void signUp(){
+        mAuth.createUserWithEmailAndPassword(edtEmail.getText().toString(),edtPassword.getText().toString()).
+                addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if (task.isSuccessful()){
+                            Toast.makeText(MainActivity.this, "SignUp Successful.", Toast.LENGTH_SHORT).show();
+
+
+                            FirebaseDatabase.getInstance().getReference().child("my_users") //Accessing the root of the Database
+                                    .child(task.getResult().getUser().getUid()) //Adding a child names my_users and getting the Unique ID
+                                    .child("username") //Adding another child
+                                    .setValue(edtUserName.getText().toString()); // setting a Value to the username
+
+                            transitionToSocialMediaActivity();
+
+                        } else{
+                            Toast.makeText(MainActivity.this, "SignUp Failed", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private void signIn(){
+        mAuth.signInWithEmailAndPassword(edtEmail.getText().toString(),edtPassword.getText().toString()).
+                addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    Toast.makeText(MainActivity.this, "SignIn Successful.", Toast.LENGTH_SHORT).show();
+                    transitionToSocialMediaActivity();
+
+                }else{
+                    Toast.makeText(MainActivity.this, "SignIn Failed.", Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
+
+    }
+
+    private void transitionToSocialMediaActivity(){
+
+        Intent intent = new Intent(MainActivity.this,SocialMediaActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
---
+++ app/src/main/java/com/example/firebasesocialmediaapp/SocialMediaActivity.java
@@ -0,0 +1,231 @@
+package com.example.firebasesocialmediaapp;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
+import java.util.UUID;
+
+public class SocialMediaActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private ImageView postImageView;
+    private Button btnCreatePost;
+    private EditText edtDescription;
+
+    private ListView usersListView;
+    private ArrayList<String> usernames;
+    private ArrayAdapter adapter;
+
+    private Bitmap bitmap;
+    private String imageIdentifier;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_social_media);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        postImageView = findViewById(R.id.postImageView);
+        btnCreatePost = findViewById(R.id.btnCreatePost);
+        edtDescription = findViewById(R.id.edtDes);
+
+        usersListView = findViewById(R.id.usersListView);
+        usernames = new ArrayList<>();
+        adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,usernames);
+        usersListView.setAdapter(adapter);
+
+        postImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectImage();
+            }
+        });
+
+        btnCreatePost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                uploadImageToServer();
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.my_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.logoutItem:
+
+
+                transitionToSignUpPage();
+
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    //when the back button is pressed it will perform a particular task
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        transitionToSignUpPage();
+    }
+
+    private void transitionToSignUpPage(){
+        mAuth.signOut();
+        Intent intent = new Intent(SocialMediaActivity.this,MainActivity.class);
+        startActivity(intent);
+        finish();
+
+    }
+
+    private void selectImage(){
+        if (Build.VERSION.SDK_INT > 23){
+            Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(intent,1000);
+        }else if (Build.VERSION.SDK_INT > 23){
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+                requestPermissions(new String[] {Manifest.permission.READ_EXTERNAL_STORAGE},1000);
+            }else{
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent,1000);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == 1000 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            selectImage();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1000 && resultCode == RESULT_OK && data != null){
+            Uri chosenImageData = data.getData();
+
+            try {
+
+                bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(),chosenImageData);
+                postImageView.setImageBitmap(bitmap);
+
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void uploadImageToServer() {
+
+        if (bitmap != null) {
+
+            // Get the data from an ImageView as bytes
+            postImageView.setDrawingCacheEnabled(true);
+            postImageView.buildDrawingCache();
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, baos);
+            byte[] data = baos.toByteArray();
+
+            imageIdentifier = UUID.randomUUID() + ".png"; // create unique values
+
+            UploadTask uploadTask = FirebaseStorage.getInstance().getReference()
+                    .child("my_images")
+                    .child(imageIdentifier)
+                    .putBytes(data);
+            uploadTask.addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception exception) {
+                    // Handle unsuccessful uploads
+                    Toast.makeText(SocialMediaActivity.this, exception.toString(), Toast.LENGTH_SHORT).show();
+                }
+            }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    // taskSnapshot.getMetadata() contains file metadata such as size, content-type, etc.
+                    // ...
+                    Toast.makeText(SocialMediaActivity.this, "Upload Successful.", Toast.LENGTH_SHORT).show();
+                    edtDescription.setVisibility(View.VISIBLE);
+
+                    FirebaseDatabase.getInstance().getReference().child("my_users").addChildEventListener(new ChildEventListener() {
+                        @Override
+                        public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+                            String username = (String) dataSnapshot.child("username").getValue();
+                            usernames.add(username);
+                            adapter.notifyDataSetChanged();
+
+                        }
+
+                        @Override
+                        public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+                        }
+
+                        @Override
+                        public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+                        }
+
+                        @Override
+                        public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                        }
+                    });
+                }
+            });
+        }
+    }
+}
